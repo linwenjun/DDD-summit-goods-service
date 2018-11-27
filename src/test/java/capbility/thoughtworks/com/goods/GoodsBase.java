01b7797f8d8987a08408f2b170f1ab7ab5b873b8
linwenjun <linwenjun@163.com>
2018-11-27 16:33:53 +0800

    add circle ci

--- src/test/java/capbility/thoughtworks/com/goods/GoodsBase.java
+++ src/test/java/capbility/thoughtworks/com/goods/GoodsBase.java
@@ -3,6 +3,8 @@ package capbility.thoughtworks.com.goods;
 import capbility.thoughtworks.com.goods.controller.GoodsController;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.WebApplicationContext;
 
 public class GoodsBase {
 
